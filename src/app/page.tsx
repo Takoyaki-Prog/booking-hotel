09d1bc6175efc6bb7223dad3fa4e97f926fe0147
Takoyaki-Prog <adif4659@gmail.com>
2025-06-14 21:23:44 +0700

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import Hero from "@/components/Hero";
+import Main from "@/components/Main";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold uppercase">Room & Rates</h1>
+          <p className="py-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, sequi.
+          </p>
+        </div>
+        <Main />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
